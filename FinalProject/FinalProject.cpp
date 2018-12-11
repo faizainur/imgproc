b71a01dbf3a5711d000ab4298d331d96c082426f
faizainur <faizainurrofiq79@gmail.com>
2018-12-11 21:49:50 +0700

    Add new img processing action

--- FinalProject/FinalProject.cpp
+++ FinalProject/FinalProject.cpp
@@ -1,16 +1,22 @@
 // FinalProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+
 #include "pch.h"
 #include <iostream>
 #include <opencv2\opencv.hpp>
 #include <Windows.h>
 
-
+// Function prototype
 int convImgMonochrome(cv::Mat img_src, std::string save_filename);
 int gaussianBlur(cv::Mat img_src, std::string save_filename);
 int homogeneusBlur(cv::Mat img_src, std::string save_filename);
+int invertImage(cv::Mat img_src, std::string save_filename);
 cv::Mat loadImage(std::string filename);
+
+// Initializing Variables
+int op_menu = 0;
+
 int main(int argc, char* argv[])
 {
 	std::string success_message;
@@ -22,7 +28,6 @@ int main(int argc, char* argv[])
 				continue;
 			}
 			else {
-
 				// Command line argument for convert image
 				if (strcmp(argv[i], "--convert") == 0) {
 					cv::Mat buff = loadImage(argv[i + 1]);
@@ -35,9 +40,8 @@ int main(int argc, char* argv[])
 						std::string save_path = argv[i + 2];
 						success_message = "Successfull converting image.\nImage saved as " + save_path;
 						MessageBox(NULL, TEXT("Successfull converting image."), TEXT("Success"), MB_ICONINFORMATION | MB_OK);
-						std::cout << success_message << std::endl;
+						std::cout << success_message << "\n[CTRL + C] to exit" << std::endl;
 						char c = (char)cv::waitKey(10);
-						system("pause");
 					}
 				}
 
@@ -54,9 +58,8 @@ int main(int argc, char* argv[])
 							std::string save_path = argv[i + 3];
 							success_message = "Successfull process image.\nImage saved as " + save_path;
 							MessageBox(NULL, TEXT("Successfull process image."), TEXT("Success"), MB_ICONINFORMATION | MB_OK);
-							std::cout << success_message << std::endl;
+							std::cout << success_message << "\n[CTRL + C] to exit" << std::endl;
 							char c = (char)cv::waitKey(10);
-							system("pause");
 						}
 					}
 					else if (strcmp(argv[i + 1], "Homogeneus Blur") == 0) {
@@ -70,22 +73,46 @@ int main(int argc, char* argv[])
 							std::string save_path = argv[i + 3];
 							success_message = "Successfull process image.\nImage saved as " + save_path;
 							MessageBox(NULL, TEXT("Successfull process image."), TEXT("Success"), MB_ICONINFORMATION | MB_OK);
-							std::cout << success_message << std::endl;
+							std::cout << success_message << "\n[CTRL + C] to exit" << std::endl;
 							char c = (char)cv::waitKey(10);
-							system("pause");
 						}
 					}
+				}
+
+				// Command line argument for invert image
+				if (strcmp(argv[i], "--threshold") == 0) {
+					std::cout << "This action still under development" << std::endl;
+
+					// Under development
+					/*cv::Mat buff = loadImage(argv[i + 1]);
+					int result = invertImage(buff, argv[i + 2]);
+					if (result > 0) {
+						MessageBox(0, TEXT("Failed to process image"), TEXT("Error"), MB_OK);
+						std::cout << "Failed to process image" << std::endl;
+					}
+					else {
+						std::string save_path = argv[i + 3];
+						success_message = "Successfull process image.\nImage saved as " + save_path;
+						MessageBox(NULL, TEXT("Successfull process image."), TEXT("Success"), MB_ICONINFORMATION | MB_OK);
+						std::cout << success_message << "([CTRL + C] to exit" << std::endl;
+						char c = (char)cv::waitKey(10);
+					}*/
 				}
 			}
 		}
 	}
-	
+	else {
+		std::cout << "ImgProc : Lightweight image processing tools" << std::endl;
+		// Menu shows
+	}
+	cv::waitKey(0);
 }
 
 cv::Mat loadImage(std::string filename) {
 	cv::Mat buff;
-	buff = imread(filename, cv::IMREAD_COLOR);
+	buff = imread(filename, cv::IMREAD_COLOR); // Load image
 
+	// Check if invalid image
 	if (buff.empty()) {
 		MessageBox(0, TEXT("Invalid image input"), TEXT("Error"), MB_OK);
 		std::cout << "Invalid image input" << std::endl;
@@ -99,8 +126,8 @@ int convImgMonochrome(cv::Mat img_src, std::string save_filename) {
 
 	cv::imwrite(save_filename, img_conv);
 
-	cv::namedWindow("Image Source", cv::WINDOW_NORMAL);
-	cv::namedWindow("Result", cv::WINDOW_NORMAL);
+	cv::namedWindow("Image Source", cv::WINDOW_AUTOSIZE);
+	cv::namedWindow("Result", cv::WINDOW_AUTOSIZE);
 
 
 
@@ -117,8 +144,8 @@ int gaussianBlur(cv::Mat img_src, std::string save_filename) {
 	
 	cv::imwrite(save_filename, img_gaussian);
 
-	cv::namedWindow("Image Source", cv::WINDOW_NORMAL);
-	cv::namedWindow("Result", cv::WINDOW_NORMAL);
+	cv::namedWindow("Image Source", cv::WINDOW_AUTOSIZE);
+	cv::namedWindow("Result", cv::WINDOW_AUTOSIZE);
 
 	cv::imshow("Image Source", img_src);
 	cv::imshow("Result", img_gaussian);
@@ -133,11 +160,30 @@ int homogeneusBlur(cv::Mat img_src, std::string save_filename) {
 
 	cv::imwrite(save_filename, img_blur);
 
-	cv::namedWindow("Image Source", cv::WINDOW_NORMAL);
-	cv::namedWindow("Result", cv::WINDOW_NORMAL);
+	cv::namedWindow("Image Source", cv::WINDOW_AUTOSIZE);
+	cv::namedWindow("Result", cv::WINDOW_AUTOSIZE);
 
 	cv::imshow("Image Source", img_src);
 	cv::imshow("Result", img_blur);
+	
+	return EXIT_SUCCESS;
+}
+
+int invertImage(cv::Mat img_src, std::string save_filename) {
+
+	// TODO : Thresholding image algorithm
+
+	cv::Mat img_invert;
+
+	cv::threshold(img_src, img_invert, 0.5, 1.0, cv::THRESH_BINARY_INV);
+
+	cv::imwrite(save_filename, img_invert);
+
+	cv::namedWindow("Image Source", cv::WINDOW_AUTOSIZE);
+	cv::namedWindow("Result", cv::WINDOW_AUTOSIZE);
+
+	cv::imshow("Image Source", img_src);
+	cv::imshow("Result", img_invert);
 
 	return EXIT_SUCCESS;
 }
